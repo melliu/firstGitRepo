d8b8ac878f7e176a043780143cb24a81d2515f49
melliu <fananliu@gmail.com>
2018-01-12 23:19:07 -0500

    'Init'

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+const express = require('express');
+const port = process.env.PORT || 3000;
+// express middle ware
+var app = express();
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log('server is up on port ', port);
+});
+
+ 
